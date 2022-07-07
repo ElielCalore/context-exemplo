d17e3e47fed410d2a904ee9892e2c6b5049506f1
Alexandre Lima <alexandreatlima@gmail.com>
2022-07-07 09:50:46 -0300

    remove upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import { NameContextComponent } from "./contexts/nameContext";
-import { FormUpload } from "./components/FormUpload";
 
 function App() {
   return (
     <NameContextComponent>
-      {/* <Header />
-      <Form /> */}
-      <FormUpload />
+      <Header />
+      <Form />
     </NameContextComponent>
   );
 }
